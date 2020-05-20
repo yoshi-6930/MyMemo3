1534fb9c62c077353c78c4f0520db19bcc39ad54
Iwamoto Yoshiki <hosomi6930@icloud.com>
2020-05-20 18:25:40 +0900

    Initial Commit

---
+++ MyMemo3/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyMemo3
+//
+//  Created by 岩元喜輝 on 2020/05/20.
+//  Copyright © 2020 Yoshiki Iwamoto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
